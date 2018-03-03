4e90080be26abcafab4a4e525bb85226f4a56fc0
BeatrizBaldaia <bibibaldaia@hotmail.com>
2018-03-03 12:35:40 +0000

    links

--- scripts/createNews.js
+++ scripts/createNews.js
@@ -1,8 +1,14 @@
 //initialize froala
-$(function() { 
+$(function() {
     $('#newsBody').froalaEditor({
         heightMin: 300,
         tabSpaces: 4,
         toolbarButtonsXS: ['bold', 'italic', 'underline', 'fontSize', 'insertImage','|', 'undo', 'redo'],
     })
  });
+
+ function createArticle(){
+     event.preventDefault();
+     console.log("Article created");
+     document.location.href = "news_user_owns.html";
+ }
