efb717b5c31235f9032337958818b1c27d11fc4f
ineeve <renatoineeve@gmail.com>
2018-03-05 21:49:26 +0000

    replaced indexSignedIn by index_signed_in, changed badges display, changed basic user to ordinary user

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 function signIn(){
     event.preventDefault();
     console.log("Sign in pressed");
-    document.location.href = "indexSignedIn.html";
+    document.location.href = "index_signed_in.html";
 }
 
 $(function () {
