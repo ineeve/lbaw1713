7719b368e282f150c9d9f5589f7ed3d4932f9954
Joao611 <joaofvde@gmail.com>
2018-03-22 14:59:54 +0000

    more pop

--- pop.sql
+++ pop.sql
@@ -165,6 +165,24 @@ INSERT INTO ReasonsForReport (reason_id, user_id, news_id) VALUES (2, 2, 1);
 INSERT INTO ReasonsForReport (reason_id, user_id, news_id) VALUES (3, 2, 1);
 INSERT INTO ReasonsForReport (reason_id, user_id, news_id) VALUES (2, 1, 2);
 INSERT INTO ReasonsForReport (reason_id, user_id, news_id) VALUES (2, 1, 3);
+INSERT INTO ReasonsForReport (reason_id, user_id, comment_id) VALUES (2, 1, 2);
+INSERT INTO ReasonsForReport (reason_id, user_id, comment_id) VALUES (2, 1, 3);
+INSERT INTO ReasonsForReport (reason_id, user_id, news_id) VALUES (1, 3, 1);
+INSERT INTO ReasonsForReport (reason_id, user_id, news_id) VALUES (2, 3, 1);
+INSERT INTO ReasonsForReport (reason_id, user_id, news_id) VALUES (3, 3, 1);
+INSERT INTO ReasonsForReport (reason_id, user_id, news_id) VALUES (1, 3, 1);
+INSERT INTO ReasonsForReport (reason_id, user_id, news_id) VALUES (2, 3, 1);
+INSERT INTO ReasonsForReport (reason_id, user_id, news_id) VALUES (3, 4, 1);
+INSERT INTO ReasonsForReport (reason_id, user_id, news_id) VALUES (2, 5, 2);
+INSERT INTO ReasonsForReport (reason_id, user_id, news_id) VALUES (2, 6, 3);
+INSERT INTO ReasonsForReport (reason_id, user_id, comment_id) VALUES (2, 6, 2);
+INSERT INTO ReasonsForReport (reason_id, user_id, comment_id) VALUES (2, 10, 3);
+INSERT INTO ReasonsForReport (reason_id, user_id, comment_id) VALUES (3, 11, 3);
+INSERT INTO ReasonsForReport (reason_id, user_id, comment_id) VALUES (2, 12, 3);
+INSERT INTO ReasonsForReport (reason_id, user_id, comment_id) VALUES (2, 15, 3);
+INSERT INTO ReasonsForReport (reason_id, user_id, news_id) VALUES (2, 12, 10);
+INSERT INTO ReasonsForReport (reason_id, user_id, comment_id) VALUES (2, 12, 25);
+INSERT INTO ReasonsForReport (reason_id, user_id, news_id) VALUES (3, 12, 3);
 
 -- Achievements
 -- TODO repeted
