f73c19939326c1d3a9506a09577154421f683396
ineeve <renatoineeve@gmail.com>
2018-04-11 19:24:50 +0100

    Fixed register CSS

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -71,7 +71,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        echo "CHEGUEI";
+        echo "Creating user";
         return User::create([
             'username' => $data['username'],
             'email' => $data['email'],
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,49 +1,58 @@
 @extends('layouts.app')
 
 @section('content')
-<form class="profile_item border container p-2 mx-auto mt-5" method="POST" action="{{ route('register') }}">
+<form class="profile_item container p-2 mx-auto mt-5" method="POST" action="{{ route('register') }}">
     {{ csrf_field() }}
     <div class="row">
       <div class="col-md-6">
+        <div class="form-group">
           <label for="username">Username</label>
-          <input id="username" type="text" name="username" value="{{ old('username') }}" required autofocus>
+          <input id="username" type="text" name="username" value="{{ old('username') }}" class="form-control" required autofocus>
           @if ($errors->has('username'))
             <span class="error">
                 {{ $errors->first('username') }}
             </span>
           @endif
-      
+        </div>
+        <div class="form-group">
           <label for="email">E-Mail Address</label>
-          <input id="email" type="email" name="email" value="{{ old('email') }}" required>
+          <input id="email" type="email" name="email" value="{{ old('email') }}" class="form-control" required>
           @if ($errors->has('email'))
             <span class="error">
                 {{ $errors->first('email') }}
             </span>
           @endif
-      
+        </div>
+        <div class="form-group">
           <label for="password">Password</label>
-          <input id="password" type="password" name="password" required>
+          <input id="password" type="password" name="password" class="form-control" required>
           @if ($errors->has('password'))
             <span class="error">
                 {{ $errors->first('password') }}
             </span>
           @endif
-      
+        </div>
+          
+        <div class="form-group">
           <label for="password-confirm">Confirm Password</label>
-          <input id="password-confirm" type="password" name="password_confirmation" required>
+          <input id="password-confirm" type="password" name="password_confirmation" class="form-control" required>
+        </div>
+          
       </div>
       <div class="col-md-6">
-
+        <div class="form-group">
           <label for="picture">Profile picture:</label>
-          <input id="picture" type="file" name="picture">
+          <input id="picture" type="file" name="picture" class="form-control">
           @if ($errors->has('picture'))
             <span class="error">
                 {{ $errors->first('picture') }}
             </span>
           @endif
-
+        </div>
+          
+        <div class="form-group">
           <label for="gender">Gender:</label>
-          <select class="custom-select" id="gender" name="gender">
+          <select class="custom-select" id="gender" name="gender" class="form-control">
             <option value="female">Female</option>
             <option value="male">Male</option>
             <option value="other">Other</option>
@@ -53,14 +62,18 @@
                 {{ $errors->first('gender') }}
             </span>
           @endif
-
+        </div>
+          
+        <div class="form-group">
           <label for="country">Country:</label>
-          <select id="country" name="country">
+          <select id="country" name="country" class="form-control">
               <option value="" disabled selected>Select your country</option>
               @foreach ($countries as $country)
                 <option value={{$country->name}}> {{$country->name}} </option>
               @endforeach
           </select>
+        </div>
+          
           
       </div>
     
