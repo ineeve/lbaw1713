56c62ff0cee2679253ce69af8afa2f662e78847f
AnabelaSilva <up201506034@fe.up.pt>
2018-04-11 17:12:52 +0100

    Criar Ajax controller

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,8 +31,8 @@
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
     crossorigin="anonymous">
-  <link rel="stylesheet" href="bootswatch.css">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="{{ asset('css/bootswatch.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
   <!-- Optional JavaScript -->
 
