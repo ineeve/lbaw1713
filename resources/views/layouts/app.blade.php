42bff455ed2a56d446334f771d568a0922a2493b
BeatrizBaldaia <bibibaldaia@hotmail.com>
2018-04-12 18:14:19 +0100

    vertical align do username

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,10 +69,12 @@
         </div>
         <div class="col-12 col-md-4">
           <div class="navbar-collapse collapse show" id="navbar">
-            @if (Auth::check())
-            <a class="button" href="{{ url('/logout') }}"> Logout </a>
-            <span>{{ Auth::user()->username }}</span>
+            
             <ul class="navbar-nav ml-auto">
+            <li class="nav-item" style="vertical-align:middle;">
+              @if (Auth::check())
+              <span class="mx-2 align-middle" style="color:white;">{{ Auth::user()->username }}</span>
+            </li>
               <!-- Publish -->
               <li class="nav-item">
                 <button class="btn btn-outline-light my-2 my-sm-0 rounded">
