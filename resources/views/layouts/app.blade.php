bee6ebbb4aa1a654c5cb25ecd4c2a132ac8915f8
João <joaofvde@gmail.com>
2018-04-12 17:03:21 +0100

    Removed A3 script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,6 @@
 
   <!-- OUR -->
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <script src="./scripts/index.js" defer></script>
 
   <!-- Font Awesome CSS -->
   <link href="https://use.fontawesome.com/releases/v5.0.7/css/all.css" rel="stylesheet">
