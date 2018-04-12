9132631d099f6b293278d4e5032270214751a9e8
João <joaofvde@gmail.com>
2018-04-12 17:39:04 +0100

    Added elfinder dependency

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //Added for TinyMCE
+        Barryvdh\Elfinder\ElfinderServiceProvider::class
+
     ],
 
     /*
--- resources/views/pages/create_news.blade.php
+++ resources/views/pages/create_news.blade.php
@@ -3,8 +3,8 @@
 <!-- <h1>HELLO WORLD!! IS IT ME???</h1> -->
 @section('title', "HELLO WORLD!!")
 @section('text_editor')
-<script src="{{asset('js/tinymce/jquery.tinymce.min.js')}}"></script>
-<script src="{{asset('js/tinymce/tinymce.min.js')}}"></script>
+<script src="{{asset('js/tinymce/js/tinymce/jquery.tinymce.min.js')}}"></script>
+<script src="{{asset('js/tinymce/js/tinymce/tinymce.min.js')}}"></script>
 
 <script>
   var editor_config = {
