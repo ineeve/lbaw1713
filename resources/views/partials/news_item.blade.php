d772e0bb4b21dd807a9693f42d551cfd022bf9c7
João <joaofvde@gmail.com>
2018-04-13 17:36:09 +0100

    fixing news options

--- resources/views/partials/news_item.blade.php
+++ resources/views/partials/news_item.blade.php
@@ -6,9 +6,11 @@
           <!-- ARTICLE OPTIONS -->
           <div class="col-1 pr-0">
             <div class="d-flex mt-5 justify-content-end">
+              <div class="container-fluid">
+              </div>
+              
               <div class="position-relative mr-2 mt-1 d-flex flex-column justify-content-between align-items-end">
                 <span> {{ $news->votes }}</span>
-                <span class="position-absolute" style="bottom: -4px;">Report</span>
               </div>
               <div class="d-flex flex-column article-options">
                 <i class="fas fa-arrow-alt-circle-up clickable-btn"></i>
@@ -16,13 +18,13 @@
                 @if (Auth::user()->id == $news->author_id)
                   <!-- Edit -->
                   <a href="edit_news.html" style="color: inherit;">
-                    <i class="fas fa-edit clickable-btn mt-2"></i>
+                    Edit <i class="fas fa-edit clickable-btn mt-2"></i>
                   </a>
                   <!-- Delete -->
-                  <i class="fas fa-times mt-2 clickable-btn mt-2"></i>
+                  Delete <i class="fas fa-times mt-2 clickable-btn mt-2"></i>
                 @else
                   <!-- Report -->
-                  <i class="fas fa-ban mt-2 clickable-btn" data-toggle="modal" data-target="#reportModal"></i>
+                  Report <i class="fas fa-ban mt-2 clickable-btn" data-toggle="modal" data-target="#reportModal"></i>
                 @endif
               </div>
             </div>
