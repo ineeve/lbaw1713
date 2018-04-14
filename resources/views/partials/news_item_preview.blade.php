c1f92b27a1ecedc8576f96903d9be2a857ba1a70
AnabelaSilva <up201506034@fe.up.pt>
2018-04-14 14:32:25 +0100

    Pictures

--- resources/views/partials/news_item_preview.blade.php
+++ resources/views/partials/news_item_preview.blade.php
@@ -2,6 +2,7 @@
     <div class="row" style="position:relative;">
         <div class="col col-sm-auto my-2">
             <img src="{{ asset('storage/news/'.$item->image) }}" width="100px" height="100px" alt="Icon">
+        
         </div>
         <div class="col">
             <div class="row">
