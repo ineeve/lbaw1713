1639d832b01b873498ee45e24d0a9b6d59355e65
João <joaofvde@gmail.com>
2018-04-14 22:48:18 +0100

    Removed unused @section for titles

--- resources/views/pages/card.blade.php
+++ resources/views/pages/card.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.app')
 
-@section('title', $card->name)
-
 @section('content')
   @include('partials.card', ['card' => $card])
 @endsection
--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.app')
 
-<!-- <h1>HELLO WORLD!! IS IT ME???</h1> -->
-@section('title', "HELLO WORLD!!")
 @section('content')
   @include('partials.news')
 @endsection
