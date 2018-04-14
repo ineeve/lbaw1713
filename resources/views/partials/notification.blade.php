fa89a63b30387ea2d4e1b3afcaf4f9f7ced7e90b
AnabelaSilva <up201506034@fe.up.pt>
2018-04-14 12:12:35 +0100

    Notification

--- resources/views/partials/notification.blade.php
+++ resources/views/partials/notification.blade.php
@@ -1,8 +1,21 @@
 <a class="dropdown-item" href="#">
     <i class="fas fa-user-circle">
     </i>
-    @if ($notification->type == 'FollowMe')
-    {{ $notification->user()->get() }} is following you! 
-    <!-- TODO: change from id to username -->
-    @endif
+    @switch($notification->type)
+    @case('FollowMe')
+        {{ $notification->user()->get()[0]->username }} is following you! 
+        @break
+    @case('CommentMyPost')
+        <!-- Todo: add -->
+        @break
+    @case('FollowedPublish')
+        <!-- Todo: add -->
+        @break
+    @case('VoteMyPost')
+        <!-- Todo: add -->
+        @break
+    @default
+        Error: in notification.
+    <!-- TODO: see -->
+@endswitch
 </a>
