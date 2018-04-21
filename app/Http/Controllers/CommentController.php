f48618ccf4f5996f3346067813ec25bae2a6d8a0
AnabelaSilva <up201506034@fe.up.pt>
2018-04-21 12:09:43 +0100

    Redirect do login

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Support\Facades\Response;
 use App\Comment;
+use Illuminate\Http\RedirectResponse;
 
 class CommentController extends Controller
 {
