8f8af7124bc6e49ded7c987eb71dcea0b361119e
ineeve <renatoineeve@gmail.com>
2018-04-25 16:58:16 +0100

    fixed some css when searching for news

---
+++ resources/views/partials/load_more.blade.php
@@ -0,0 +1,10 @@
+<div class="row">
+    <div class="col">
+    <a id="scrollNewsPreview" href="#scrollNewsPreview" class="loadMore" style="text-decoration: none;">Show More</a>
+    </div>
+    <div class="col text-right">
+    <p class="totop">
+        <a style="text-decoration: none;" href="#top">Back to top</a>
+    </p>
+    </div>
+</div>
--- resources/views/partials/news.blade.php
+++ resources/views/partials/news.blade.php
@@ -28,16 +28,7 @@
                 @include('partials.news_item_preview_list',$news)
               @endif
             </div>
-            <div class="row">
-              <div class="col">
-                <a id="scrollNewsPreview" href="#scrollNewsPreview" class="loadMore" style="text-decoration: none;">Show More</a>
-              </div>
-              <div class="col text-right">
-                <p class="totop">
-                  <a style="text-decoration: none;" href="#top">Back to top</a>
-                </p>
-              </div>
-            </div>
+            @include('partials.load_more')
           </div>
         </div>
       </div>
