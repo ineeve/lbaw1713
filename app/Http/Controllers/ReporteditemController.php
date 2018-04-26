05de5596d1042c37cfb8f78d8e34db99c484558f
AnabelaSilva <up201506034@fe.up.pt>
2018-04-26 18:09:22 +0100

    forum

--- app/Http/Controllers/ReporteditemController.php
+++ app/Http/Controllers/ReporteditemController.php
@@ -11,8 +11,14 @@ class ReporteditemController extends Controller
 {
     public function show() {
         //get All news Reports
-        $reports = DB::select('SELECT * FROM reporteditems JOIN news ON(news.id=reporteditems.news_id)
-        JOIN users ON(news.author_id=users.id)WHERE comment_id IS NULL');
+        $reports = DB::select('SELECT news.title, users.username, news.date AS date, count(reporteditems.id) AS reports_number 
+		FROM reporteditems 
+        JOIN news ON(news.id = reporteditems.news_id)
+        JOIN users ON(news.author_id=users.id)
+		JOIN reporteditems AS r ON(reporteditems.id=r.id)
+        WHERE reporteditems.comment_id IS NULL
+        GROUP BY users.username, news.title, news.date
+        ORDER BY COUNT(reporteditems.id) DESC;');
         return view('pages.reports',['reports' => $reports]);
     }
 }
