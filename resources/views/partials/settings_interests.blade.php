fece1bf85575431530af3818a7ef9a4430d53260
João <joaofvde@gmail.com>
2018-04-29 16:03:19 +0100

    Modular settings view

---
+++ resources/views/partials/settings_interests.blade.php
@@ -0,0 +1,29 @@
+<div class="tab-pane fade active show" id="interests">
+  <h3>Manage Interests</h3>
+  <h4 class="pt-3">Add A New Interest</h4>
+  <form class="form-inline pb-3">
+    <input list="list_interests" name="interest" placeholder="Interest" class="form-control">
+    <datalist id="list_interests">
+      <option value="Sports"></option>
+      <option value="Politics"></option>
+      <option value="Business"></option>
+      <option value="Technology"></option>
+      <option value="Science"></option>
+    </datalist>
+    <input class="btn ml-2" value="Add" type="submit">
+  </form>
+  <div class="list-group">
+    <a href="#" class="list-group-item list-group-item-action">Animals
+      <div class="float-right">X</div>
+    </a>
+    <a href="#" class="list-group-item list-group-item-action">Music
+      <div class="float-right">X</div>
+    </a>
+    <a href="#" class="list-group-item list-group-item-action">Food
+      <div class="float-right">X</div>
+    </a>
+    <a href="#" class="list-group-item list-group-item-action">Travel
+      <div class="float-right">X</div>
+    </a>
+  </div>
+</div>
---
+++ resources/views/partials/settings_notifications.blade.php
@@ -0,0 +1,31 @@
+<div class="tab-pane fade" id="notifications">
+  <h3>Manage Notifications</h3>
+  <fieldset class="pt-3">
+    <div class="form-group">
+      <div class="custom-control custom-checkbox">
+        <input class="custom-control-input" type="checkbox" value="" id="article-comments">
+        <label class="custom-control-label" for="article-comments">
+          Comments To My Articles
+        </label>
+      </div>
+      <div class="custom-control custom-checkbox">
+        <input class="custom-control-input" type="checkbox" value="" id="article-votes">
+        <label class="custom-control-label" for="article-votes">
+          Votes In My Articles
+        </label>
+      </div>
+      <div class="custom-control custom-checkbox">
+        <input class="custom-control-input" type="checkbox" value="" id="follow-articles" checked>
+        <label class="custom-control-label" for="follow-articles">
+          Articles from people I follow
+        </label>
+      </div>
+      <div class="custom-control custom-checkbox">
+        <input class="custom-control-input" type="checkbox" value="" id="article-interest">
+        <label class="custom-control-label" for="article-interest">
+          New articles of my interest
+        </label>
+      </div>
+    </div>
+  </fieldset>
+</div>
