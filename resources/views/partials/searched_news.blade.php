d8f31ee320f1ae93b6594949c58b9092592e4e73
ineeve <renatoineeve@gmail.com>
2018-04-29 16:17:24 +0100

    fixed searched_news partial

--- resources/views/partials/searched_news.blade.php
+++ resources/views/partials/searched_news.blade.php
@@ -10,14 +10,14 @@
         </div>
         <div class="d-flex flex-column">
             <div id="news_item_preview_list">
-              @if ($news != null && count($news) > 0)
+              @if (isset($news) && count($news) > 0)
                 @include('partials.news_item_preview_list',$news)
               @else
                 <hr>
                 <h4>Sorry, no news matched your search. Try with different keywords or with advanced search.</h4>
               @endif
             </div>
-            @if($news != null && count($news > 0))
+            @if(isset($news) && count($news) > 0)
               @include('partials.load_more')
             @endif
           </div>
