62fd5a58885c54c5d86a7f2e918994e44c112054
BeatrizBaldaia <bibibaldaia@hotmail.com>
2018-04-30 12:03:43 +0100

    paginação de pessoas que sigo

--- resources/views/partials/articles_pagination_btn.blade.php
+++ resources/views/partials/articles_pagination_btn.blade.php
@@ -1,13 +1,13 @@
 <ul class="pagination">
     <li class="page-item mx-2">
-        @if($offset == 0)
+        @if($articles_offset == 0)
         <i class="fas fa-2x fa-angle-left clickable-btn" id="previous_articles" onclick="getPreviousArticles( '{{ $user->username }}' )"></i>
         @else
         <i class="fas fa-2x fa-angle-left clickable-btn clickable" id="previous_articles" onclick="getPreviousArticles( '{{ $user->username }}' )"></i>
         @endif
     </li>
     <li class="page-item mx-2">
-        @if($count
+        @if($articles_count
         <=0 ) <i class="fas fa-2x fa-angle-right clickable-btn" id="next_articles" onclick="getNextArticles( '{{ $user->username }} )"></i>
             @else
             <i class="fas fa-2x fa-angle-right clickable-btn clickable" id="next_articles" onclick="getNextArticles( '{{ $user->username }}' )"></i>
