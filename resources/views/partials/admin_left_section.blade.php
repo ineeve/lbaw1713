ff2fa52babfbbb3a55328395bf1a6be85f9bd98a
João <joaofvde@gmail.com>
2018-05-21 12:29:02 +0100

    collapse admin sidebar on smaller screens

--- resources/views/partials/admin_left_section.blade.php
+++ resources/views/partials/admin_left_section.blade.php
@@ -1,9 +1,11 @@
+<script src="{{ asset('js/collapsible.js') }}" defer></script>
+
 <!-- LEFT SECTIONS -->
 <div class="col-xl-2 col-lg-3 col-12">
     <div class="col-lg-12 col-sm-6 col-12 p-0">
         <h3 data-toggle="collapse" data-target="#sections_list" aria-expanded="true" aria-controls="sections_list">
             <i class="fas fa-bars"></i> Sections</h3>
-        <div class="collapse" id="sections_list">
+        <div class="collapse show collapsible-lg" id="sections_list">
             <ul class="nav nav-pills flex-column left-pane">
                 <li class="nav-item">
                     <a href="#users" role="button" data-toggle="tab" class="nav-link active">
