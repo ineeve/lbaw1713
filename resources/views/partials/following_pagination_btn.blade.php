4491fb3d84f6478c61ed9ed2d89501e38ca573d8
AnabelaSilva <up201506034@fe.up.pt>
2018-05-28 14:28:12 +0100

    correcoes

--- resources/views/partials/following_pagination_btn.blade.php
+++ resources/views/partials/following_pagination_btn.blade.php
@@ -1,12 +1,12 @@
 <nav aria-label="...">
 
 <ul id="profile_following_pag" class="pagination">
-    <li class="page-item" value='1'>
+    <li class="page-item" data-value='1'>
         <a class="page-link" href="#" data-value="first">First</a>
     </li>
     @if(floor($following_offset/5 + 1)<5)
         @for($i=1;$i<floor($following_offset/5 + 1);$i++)
-        <li class="page-item" value={{$i}}>
+        <li class="page-item" data-value={{$i}}>
             <a class="page-link" href="#" data-value={{$i}}>{{$i}} </a>
         </li>
         @endfor
@@ -15,16 +15,16 @@
             <span class="page-link">...</span>
         </li>
         @for($i=floor($following_offset/5 + 1)-3;$i< floor($following_offset/5 + 1);$i++)
-        <li class="page-item" value={{$i}}>
+        <li class="page-item" data-value={{$i}}>
             <a class="page-link" href="#" data-value={{$i}}>{{$i}}</a>
         </li>
         @endfor
     @endif
-        <li class="page-item active" value={{$i}}>
+        <li class="page-item active" data-value={{$i}}>
             <a class="page-link" href="#" data-value={{$i}}>{{floor($following_offset/5 + 1)}}</a>
         </li>
         @for($i=floor($following_offset/5 + 1)+1;$i< floor($following_offset/5 + 1)+4&&$i<=$following_count;$i++) 
-            <li class="page-item" value={{$i}}>
+            <li class="page-item" data-value={{$i}}>
             <a class="page-link" href="#" data-value={{$i}}>{{$i}}</a>
             </li>
         @endfor 
@@ -33,7 +33,7 @@
                 <span class="page-link">...</span>
             </li>
             @endif
-            <li class="page-item" value={{$following_count}}>
+            <li class="page-item" data-value={{$following_count}}>
                 <a class="page-link" href="#" data-value='last'>Last</a>
             </li>
 </ul>
