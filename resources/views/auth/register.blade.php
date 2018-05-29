4be053d1700de4edd5d3ad56c35e6e3726304e55
ineeve <renatoineeve@gmail.com>
2018-05-29 16:59:12 +0100

    Added register help

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+
 <form class="profile_item container p-2 mx-auto mt-5" enctype="multipart/form-data" method="POST" action="{{ route('register') }}">
     {{ csrf_field() }}
     <div class="row">
+      <div class="col-12">
+        <span title="Only username, email and password are required">General Help <i class="far fa-question-circle fa-fw"></i></span>
+      </div>
       <div class="col-md-6">
         <fieldset class="form-group">
           <legend>Account details</legend>
-          <label for="username">Username:</label>
+          <label for="username"><i title="Your username will be public to other users. It should not be over 30 characters." class="far fa-question-circle fa-fw"></i>Username:</label>
           @if ($errors->has('username'))
             <span class="error">
               <i class="fas fa-exclamation-triangle"></i> {{ $errors->first('username') }}
@@ -16,7 +20,7 @@
           <input id="username" type="text" name="username" value="{{ old('username') }}" class="form-control" required autofocus>
 
 
-          <label for="email">E-Mail Address:</label>
+          <label for="email"><i title="Your email will be public to other users. It can be used to login and to recover your password later." class="far fa-question-circle fa-fw"></i> E-Mail Address:</label>
           @if ($errors->has('email'))
             <span class="error">
               <i class="fas fa-exclamation-triangle"></i> {{ $errors->first('email') }}
@@ -25,7 +29,7 @@
           <input id="email" type="email" name="email" value="{{ old('email') }}" class="form-control" required>
 
           
-          <label for="password">Password:</label>
+          <label for="password"><i class="far fa-question-circle fa-fw" title="Your password should be secure. Our only restriction is to have more than 6 characters"></i>Password:</label>
           @if ($errors->has('password'))
             <span class="error">
               <i class="fas fa-exclamation-triangle"></i> {{ $errors->first('password') }}
@@ -41,7 +45,7 @@
       <div class="col-md-6">
         <fieldset class="form-group">
           <legend>Personal details</legend>
-          <label for="picture">Profile picture:</label>
+          <label for="picture"><i class="far fa-question-circle fa-fw" title="You are not required to upload a picture, we provide a default one. The picture will be publicly displayed in your profile."></i>Profile picture:</label>
           @if ($errors->has('picture'))
             <span class="error">
               <i class="fas fa-exclamation-triangle"></i> {{ $errors->first('picture') }}
@@ -49,13 +53,13 @@
           @endif
           <input id="picture" type="file" name="picture" class="form-control">
 
-          <label for="gender">Gender:</label>
+          <label for="gender"><i class="far fa-question-circle fa-fw" title="You are not required to set your gender, although we provide an 'other' option. If you select one, it will be publicly displayed in your profile"></i>Gender:</label>
           @if ($errors->has('gender'))
             <span class="error">
               <i class="fas fa-exclamation-triangle"></i> {{ $errors->first('gender') }}
             </span>
           @endif
-          <select class="custom-select" id="gender" name="gender" required="required">
+          <select class="custom-select" id="gender" name="gender">
             <option value="" disabled selected>Select your gender</option>
             <option value="female">Female</option>
             <option value="male">Male</option>
@@ -63,7 +67,7 @@
           </select>
 
           
-          <label for="country">Country:</label>
+          <label for="country"><i class="far fa-question-circle fa-fw" title="You are not required to set your country. If you select one, it will be publicly displayed in your profile."></i>Country:</label>
           @if ($errors->has('country_id'))
             <span class="error">
               <i class="fas fa-exclamation-triangle"></i> {{ $errors->first('country_id') }}
@@ -85,5 +89,6 @@
     <button class="btn btn-primary ml-3" type="submit">
       Register
     </button>
+    </div>
 </form>
 @endsection
