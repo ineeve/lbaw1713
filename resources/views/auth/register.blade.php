eaf5c35b948efe02564819d3f045ad84834bafcb
ineeve <renatoineeve@gmail.com>
2018-05-29 15:42:33 +0100

    Added color to error. Changed type of Users.username to CITEXT

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,26 +8,31 @@
         <fieldset class="form-group">
           <legend>Account details</legend>
           <label for="username">Username:</label>
-          <input id="username" type="text" name="username" value="{{ old('username') }}" class="form-control" required autofocus>
           @if ($errors->has('username'))
             <span class="error">
                 {{ $errors->first('username') }}
             </span>
           @endif
+          <input id="username" type="text" name="username" value="{{ old('username') }}" class="form-control" required autofocus>
+
+
           <label for="email">E-Mail Address:</label>
-          <input id="email" type="email" name="email" value="{{ old('email') }}" class="form-control" required>
           @if ($errors->has('email'))
             <span class="error">
                 {{ $errors->first('email') }}
             </span>
           @endif
+          <input id="email" type="email" name="email" value="{{ old('email') }}" class="form-control" required>
+
+          
           <label for="password">Password:</label>
-          <input id="password" type="password" name="password" class="form-control" required>
           @if ($errors->has('password'))
             <span class="error">
                 {{ $errors->first('password') }}
             </span>
           @endif
+          <input id="password" type="password" name="password" class="form-control" required>
+
           
           <label for="password-confirm">Confirm Password:</label>
           <input id="password-confirm" type="password" name="password_confirmation" class="form-control" required>
@@ -37,38 +42,40 @@
         <fieldset class="form-group">
           <legend>Personal details</legend>
           <label for="picture">Profile picture:</label>
-          <input id="picture" type="file" name="picture" class="form-control">
           @if ($errors->has('picture'))
             <span class="error">
                 {{ $errors->first('picture') }}
             </span>
           @endif
+          <input id="picture" type="file" name="picture" class="form-control">
 
           <label for="gender">Gender:</label>
+          @if ($errors->has('gender'))
+            <span class="error">
+                {{ $errors->first('gender') }}
+            </span>
+          @endif
           <select class="custom-select" id="gender" name="gender" required="required">
             <option value="" disabled selected>Select your gender</option>
             <option value="female">Female</option>
             <option value="male">Male</option>
             <option value="other">Other</option>
           </select>
-          @if ($errors->has('gender'))
-            <span class="error">
-                {{ $errors->first('gender') }}
-            </span>
-          @endif
+
           
           <label for="country">Country:</label>
+          @if ($errors->has('country_id'))
+            <span class="error">
+                {{ $errors->first('country_id') }}
+            </span>
+          @endif
           <select id="country" name="country_id" class="form-control">
               <option value="" disabled selected>Select your country</option>
               @foreach ($countries as $country)
                 <option value="{{$country->id}}"> {{$country->name}} </option>
               @endforeach
           </select>
-          @if ($errors->has('country_id'))
-            <span class="error">
-                {{ $errors->first('country_id') }}
-            </span>
-          @endif
+
         </fieldset>
           
           
