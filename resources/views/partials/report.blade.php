dae7edec792209862cbe2406d5b3db86ffa95d3f
BeatrizBaldaia <bibibaldaia@hotmail.com>
2018-05-29 17:04:53 +0100

    ola

--- resources/views/partials/report.blade.php
+++ resources/views/partials/report.blade.php
@@ -4,6 +4,13 @@
       <strong>News' title</strong>
     </h2>
   </div>
+  @if($isReportedComment)
+  <div class="col">
+      <h2>
+        <strong>Reported comment</strong>
+      </h2>
+    </div>
+    @endif
   <div class="col">
     <h2>
       <strong>Author's name</strong>
@@ -17,17 +24,24 @@
 </div>
 
 <div class="row text-center m-5">
-  <div class="col">
+  <div title="{{$info->title_link}}" class="col" style="width: 20%">
     <a class="nounderline" style="color:inherit;" href="{{ url('news/'.$info->news_id) }}">
-      <h4>{{$info->title}}</h4>
+      <h4 style="width: 90%; text-overflow: ellipsis; white-space: nowrap; overflow: hidden;">
+        {{$info->title_link}}</h4>
     </a>
   </div>
-  <div class="col">
+  @if($isReportedComment)
+  <div title="{{$info->text}}" class="col" style="width: 20%">
+      <h4 style="width: 90%; text-overflow: ellipsis; white-space: nowrap; overflow: hidden;"
+      >{{$info->text}}</h4>
+  </div>
+  @endif
+  <div class="col" style="width: 20%">
     <a class="nounderline" style="color:inherit;" href="{{ url('users/'.$info->username) }}">
       <h4>{{$info->username}}</h4>
     </a>
   </div>
-  <div class="col">
+  <div class="col" style="width: 20%">
     <h4>{{ date("F jS, Y \a\\t H:i", strtotime($info->date)) }}</h4>
   </div>
 </div>
