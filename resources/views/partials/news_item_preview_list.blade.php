dce95dedb8a9934faf5e2cd53b10c10874806dc3
ineeve <renatoineeve@gmail.com>
2018-05-29 18:44:54 +0100

    Added hidden link in header poiting to content

--- resources/views/partials/news_item_preview_list.blade.php
+++ resources/views/partials/news_item_preview_list.blade.php
@@ -1,7 +1,7 @@
 @if(is_null($news) || count($news) == 0)
 <div class="container ml-0">
     <div class="alert alert-primary alert-dismissible fade show" role="alert">
-    <strong>Sorry!</strong> There are no news to show.
+    <strong>Sorry!</strong> There are no news to show in this section.
     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
     </button>
