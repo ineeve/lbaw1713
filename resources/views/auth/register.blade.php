d4b3cf1d73189c0892a45bcbe8bc2bf038da8eed
ineeve <renatoineeve@gmail.com>
2018-05-29 15:08:23 +0100

    Added fieldsets to register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,42 +5,37 @@
     {{ csrf_field() }}
     <div class="row">
       <div class="col-md-6">
-        <div class="form-group">
-          <label for="username">Username</label>
+        <fieldset class="form-group">
+          <legend>Account details</legend>
+          <label for="username">Username:</label>
           <input id="username" type="text" name="username" value="{{ old('username') }}" class="form-control" required autofocus>
           @if ($errors->has('username'))
             <span class="error">
                 {{ $errors->first('username') }}
             </span>
           @endif
-        </div>
-        <div class="form-group">
-          <label for="email">E-Mail Address</label>
+          <label for="email">E-Mail Address:</label>
           <input id="email" type="email" name="email" value="{{ old('email') }}" class="form-control" required>
           @if ($errors->has('email'))
             <span class="error">
                 {{ $errors->first('email') }}
             </span>
           @endif
-        </div>
-        <div class="form-group">
-          <label for="password">Password</label>
+          <label for="password">Password:</label>
           <input id="password" type="password" name="password" class="form-control" required>
           @if ($errors->has('password'))
             <span class="error">
                 {{ $errors->first('password') }}
             </span>
           @endif
-        </div>
           
-        <div class="form-group">
-          <label for="password-confirm">Confirm Password</label>
+          <label for="password-confirm">Confirm Password:</label>
           <input id="password-confirm" type="password" name="password_confirmation" class="form-control" required>
-        </div>
-          
+        </fieldset>
       </div>
       <div class="col-md-6">
-        <div class="form-group">
+        <fieldset class="form-group">
+          <legend>Personal details</legend>
           <label for="picture">Profile picture:</label>
           <input id="picture" type="file" name="picture" class="form-control">
           @if ($errors->has('picture'))
@@ -48,9 +43,7 @@
                 {{ $errors->first('picture') }}
             </span>
           @endif
-        </div>
-          
-        <div class="form-group">
+
           <label for="gender">Gender:</label>
           <select class="custom-select" id="gender" name="gender" required="required">
             <option value="" disabled selected>Select your gender</option>
@@ -63,9 +56,7 @@
                 {{ $errors->first('gender') }}
             </span>
           @endif
-        </div>
           
-        <div class="form-group">
           <label for="country">Country:</label>
           <select id="country" name="country_id" class="form-control">
               <option value="" disabled selected>Select your country</option>
@@ -78,7 +69,7 @@
                 {{ $errors->first('country_id') }}
             </span>
           @endif
-        </div>
+        </fieldset>
           
           
       </div>
