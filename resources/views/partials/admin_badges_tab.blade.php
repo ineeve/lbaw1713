b5bbc17a6215a3111eb075307e5d2ba7d906bca8
João <joaofvde@gmail.com>
2018-05-30 05:05:32 +0100

    add badge working

--- resources/views/partials/admin_badges_tab.blade.php
+++ resources/views/partials/admin_badges_tab.blade.php
@@ -3,15 +3,15 @@
 <div class="mb-2">
     <div class="row">
         <div class="col-12 col-md-11">
-            <h3><i class="fa fa-flask fa-fw"></i> Badges</h3>
+            <h3>
+                <i class="fa fa-flask fa-fw"></i> Badges</h3>
             <!-- Badges listing -->
             <div id="badges-list" class="mt-2 d-flex flex-wrap align-items-center">
-                @foreach ($badges as $badge)
-                    @include('partials.admin_badge', ['badge' => $badge])
-                @endforeach
-                <!-- <div class="text-primary" data-toggle="modal" data-target="#addCategoryModal">
-                    @include('partials.admin_category', ['cat_id' => NULL, 'cat_icon' => 'fas fa-plus-circle', 'cat_name' => 'Add Category', 'create_cat' => true])
-                </div> -->
+                @foreach ($badges as $badge) @include('partials.admin_badge', ['badge' => $badge]) @endforeach
+                <a href="" class="d-flex flex-column align-items-center nounderline mt-3 mr-3" style="min-width:16rem;">
+                    <i class="fas fa-plus-circle fa-fw medium-big-icon"></i>
+                    <p class="m-0">Add Badge</p>
+                </a>
             </div>
         </div>
     </div>
