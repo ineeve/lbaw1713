55461e8db46ede615183e270b36398dde8cc7a84
ineeve <renatoineeve@gmail.com>
2018-05-30 02:12:48 +0100

    Added labels and required to advanced search modal

--- resources/views/partials/modals/search_modal.blade.php
+++ resources/views/partials/modals/search_modal.blade.php
@@ -13,10 +13,11 @@
         <form action="{{route('advanced_search')}}" method="get">
           <div class="form-group">
             <label for="advancedsearch-searchText">Search Text *</label>
-            <input id="advancedsearch-searchText"type="text" class="form-control" placeholder="Search" name="searchText">
+            <input id="advancedsearch-searchText"type="text" class="form-control" placeholder="Search" name="searchText" required>
           </div>
           <div class="form-group my-1">
-            <select class="custom-select" name="elementToSearch">
+            <label for="advancedsearch-typeOfSearch">Type of search</label>
+            <select id="advancedsearch-typeOfSearch"class="custom-select" name="elementToSearch">
               <option value="titleAndBody" selected>Search for...</option>
               <option value="onlyTitle">Only title</option>
               <option value="onlyBody">Only body</option>
@@ -25,27 +26,30 @@
             </select>
           </div>
           <div class="form-group my-1">
-            <select name="sectionSearch" class="custom-select">
+            <label for="avancedsearch-sectionSearch">Sections</label>
+            <select id="avancedsearch-sectionSearch" name="sectionSearch" class="custom-select">
               <option value="">All</option>
               @foreach($sections as $section)
               <option value="{{ $section->id }}">{{ $section->name }}</option>
               @endforeach
             </select>
           </div>
-          <input type="text" name="authorSearch" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="News' Author...">
+          <label for="advancedsearch-author">News' author</label>
+          <input type="text" name="authorSearch" class="form-control mb-2 mr-sm-2 mb-sm-0" id="advancedsearch-author" placeholder="News' Author...">
           <div class="d-flex">
             <div class="form-group">
               <!-- <p><time></time></p> -->
-              <label class="col-form-label">Begin:</label>
-              <input type="date" name="date1" class="form-control" value="1990-01-01">
+              <label for="advancedsearch-beginDate" class="col-form-label">Begin</label>
+              <input id="advancedsearch-beginDate" type="date" name="date1" class="form-control" value="1990-01-01">
             </div>
             <div class="form-group">
               <!-- <p><time></time></p> -->
-              <label class="col-form-label">End:</label>
-              <input type="date" name="date2" class="form-control" value="2018-03-15">
+              <label for="advancedsearch-endDate" class="col-form-label">End</label>
+              <input id="advancedsearch-endDate" type="date" name="date2" class="form-control" value="2018-03-15">
             </div>
           </div>
           <button type="submit" class="btn btn-primary float-right">Search</button>
+          <div class="mt-2">* Field is required</div>
         </form>
       </div>
     </div>
