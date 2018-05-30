b58ad5b9276ca86ba595f2f3555ccf2383cafba8
ineeve <renatoineeve@gmail.com>
2018-05-30 05:58:52 +0100

    admin category

--- resources/views/partials/admin_category.blade.php
+++ resources/views/partials/admin_category.blade.php
@@ -6,7 +6,7 @@ if ($create_cat) {
 }
 ?>
 
-<div class="news_box d-flex flex-column flex-wrap align-items-center p-3" @if(!$create_cat) onclick="setEdit({{$cat_id}}, '{{$cat_name}}', '{{$cat_icon}}')" @endif>
+<div class="news_box d-flex flex-column flex-wrap align-items-center p-3" style="width:16rem;" @if(!$create_cat) onclick="setEdit({{$cat_id}}, '{{$cat_name}}', '{{$cat_icon}}')" @endif>
     <i class="{{ $cat_icon }} fa-fw {{$icon_size}}"></i>
     <p class="m-0">{{ $cat_name }}</p>
 </div>
