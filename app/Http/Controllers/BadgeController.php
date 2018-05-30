82616146bbc22b14f10b2d58a6ea67fc63d070e8
João <joaofvde@gmail.com>
2018-05-30 03:01:50 +0100

    create badge backend

--- app/Http/Controllers/BadgeController.php
+++ app/Http/Controllers/BadgeController.php
@@ -9,6 +9,18 @@ use Auth;
 
 class BadgeController extends Controller {
     
+    function create(Request $request) {
+        $this->authorize('admin', Auth::user());
+        $badge = new Badge;
+        $badge->name = $request->name;
+        $badge->brief = $request->brief;
+        $badge->votes = $request->votes;
+        $badge->articles = $request->articles;
+        $badge->comments = $request->comments;
+        $badge->save();
+        return view('partials.admin_page', ['badge' => $badge]);
+    }
+
     function showAdmin($badge_id) {
         $this->authorize('admin', Auth::user());
         $badge = Badge::find($badge_id);
